23c18b713f8da2b0cbd7db2195dab7e2e883eae5
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-08-28 14:09:38 +0200

    obsolete types

---
+++ src/NServiceBus.Autofac/obsoletes.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus
+{
+    // An internal type referenced by the API approvals test as it can't reference obsoleted types.
+    class AutofacInternalType
+    {
+    }
+
+    /// <summary>
+    /// Autofac Container
+    /// </summary>
+    [ObsoleteEx(
+        Message = "NServiceBus.Autofac is no longer supported. Use the externally managed container mode to integrate third party dependency injection containers with NServiceBus instead.",
+        RemoveInVersion = "9.0.0",
+        TreatAsErrorFromVersion = "8.0.0")]
+    public class AutofacBuilder
+    {
+    }
+
+    /// <summary>
+    /// Autofac extension to pass an existing Autofac container instance.
+    /// </summary>
+    [ObsoleteEx(
+        Message = "NServiceBus.Autofac is no longer supported. Use the externally managed container mode to integrate third party dependency injection containers with NServiceBus instead.",
+        RemoveInVersion = "9.0.0",
+        TreatAsErrorFromVersion = "8.0.0")]
+    public static class AutofacExtensions
+    {
+    }
+}
