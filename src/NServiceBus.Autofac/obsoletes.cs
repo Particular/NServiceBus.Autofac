fe297e118cc3dc9cab97b62f610763c462c9fea3
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-08-31 14:30:35 +0200

    Obsolete NServiceBus.Autofac (#133)
    * switch to .NET Framework 4.7.2
    
    * +semver: major
    
    * remove acceptance tests
    
    * obsolete types
    
    * remove obsolete tests
    
    * update api apprvovals
    
    * update obsolete message

---
+++ src/NServiceBus.Autofac/obsoletes.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus
+{
+    // An internal type referenced by the API approvals test as it can't reference obsoleted types.
+    class AutofacInternalType
+    {
+    }
+
+    /// <summary>
+    /// Autofac Container
+    /// </summary>
+    [ObsoleteEx(
+        Message = "Autofac is no longer supported via the NServiceBus.Autofac adapter. NServiceBus directly supports all containers compatible with Microsoft.Extensions.DependencyInjection.Abstractions via the externally managed container mode.",
+        RemoveInVersion = "9.0.0",
+        TreatAsErrorFromVersion = "8.0.0")]
+    public class AutofacBuilder
+    {
+    }
+
+    /// <summary>
+    /// Autofac extension to pass an existing Autofac container instance.
+    /// </summary>
+    [ObsoleteEx(
+        Message = "Autofac is no longer supported via the NServiceBus.Autofac adapter. NServiceBus directly supports all containers compatible with Microsoft.Extensions.DependencyInjection.Abstractions via the externally managed container mode.",
+        RemoveInVersion = "9.0.0",
+        TreatAsErrorFromVersion = "8.0.0")]
+    public static class AutofacExtensions
+    {
+    }
+}
