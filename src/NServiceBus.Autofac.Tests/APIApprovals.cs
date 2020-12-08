c45b59e5c76c38f2a3815c701bb03be2792e2c43
Sean Feldman <feldman.sean@gmail.com>
2020-12-08 11:13:35 -0700

    Fix API Approvals

--- src/NServiceBus.Autofac.Tests/APIApprovals.cs
+++ src/NServiceBus.Autofac.Tests/APIApprovals.cs
@@ -9,7 +9,10 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(AutofacInternalType).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = typeof(AutofacInternalType).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
         Approver.Verify(publicApi);
     }
 }
